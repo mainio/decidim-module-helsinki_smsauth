c91fffcdb1434168009d3851374afb3287991f46
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-08-29 14:50:22 +0300

    Add migration for the sign in codes

---
+++ db/migrate/20230829113036_create_decidim_helsinki_smsauth_signin_code_sets.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateDecidimHelsinkiSmsauthSigninCodeSets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :decidim_helsinki_smsauth_signin_code_sets do |t|
+      t.jsonb :metadata
+      t.integer :gererated_code_amount
+      t.integer :used_code_amount
+      t.references :decidim_user, null: false, index: { name: "index_helsinki_smsauth_code_sets_on_decidim_user" }
+
+      t.datetime :created_at
+    end
+  end
+end
