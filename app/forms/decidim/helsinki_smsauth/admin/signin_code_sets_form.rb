3e014680b3c468728c083ea9da2024b1f270efcd
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-08-29 15:29:19 +0300

    Move admin form to the admin folder

---
+++ app/forms/decidim/helsinki_smsauth/admin/signin_code_sets_form.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Decidim
+  module HelsinkiSmsauth
+    module Admin
+      class SigninCodeSetsForm < Form
+        mimic :signin_codes
+
+        attribute :gererated_code_amount, Integer
+        attribute :used_code_amount, Integer
+        attribute :user, String
+
+        validates :gererated_code_amount, numericality: { greater_than: 0 }
+        validates :used_code_amount, numericality: { greater_than_or_equal_to: 0 }
+        validates :user, presence: true
+      end
+    end
+  end
+end
