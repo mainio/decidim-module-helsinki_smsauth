12e09f9eaed11b6f8529631892bcd925ac192c75
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-08-29 17:00:05 +0300

    Fix a typo in a migration

--- db/migrate/20230829113036_create_decidim_helsinki_smsauth_signin_code_sets.rb
+++ db/migrate/20230829113036_create_decidim_helsinki_smsauth_signin_code_sets.rb
@@ -4,7 +4,7 @@ class CreateDecidimHelsinkiSmsauthSigninCodeSets < ActiveRecord::Migration[6.1]
   def change
     create_table :decidim_helsinki_smsauth_signin_code_sets do |t|
       t.jsonb :metadata
-      t.integer :gererated_code_amount
+      t.integer :generated_code_amount
       t.integer :used_code_amount
       t.references :decidim_user, null: false, index: { name: "index_helsinki_smsauth_code_sets_on_decidim_user" }
 
