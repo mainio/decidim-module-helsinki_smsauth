ce3e5ce632560e1631d3d8752fa85c62d9009e67
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-09-20 15:48:11 +0300

    Add factory for signin code and signin code set

--- lib/decidim/helsinki_smsauth/test/factories.rb
+++ lib/decidim/helsinki_smsauth/test/factories.rb
@@ -3,5 +3,22 @@
 require "decidim/core/test/factories"
 
 FactoryBot.define do
-  # Add engine factories here
+  factory :signin_code_set, class: "Decidim::HelsinkiSmsauth::SigninCodeSet" do
+    creator { create(:user, :confirmed, :admin, organization: organization) }
+    generated_code_amount { 1 }
+    metadata do
+      {
+        school: Faker::Name.name,
+        grade: Faker::Number.between(from: 1, to: 6)
+      }
+    end
+
+    after(:create) do |signin_code_set, evaluator|
+      create_list(:signin_code, evaluator.generated_code_amount, signin_code_set: signin_code_set)
+    end
+  end
+
+  factory :signin_code, class: "Decidim::HelsinkiSmsauth::SigninCode" do
+    signin_code_set { create(signin_code_set) }
+  end
 end
