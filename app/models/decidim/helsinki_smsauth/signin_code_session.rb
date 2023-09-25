04f19b06336baf18ddd7de27df544cdc7ebccb9d
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-09-25 19:58:58 +0300

    Fix the sign in codes generation with the privacy module

--- app/models/decidim/helsinki_smsauth/signin_code_session.rb
+++ app/models/decidim/helsinki_smsauth/signin_code_session.rb
@@ -3,7 +3,7 @@
 module Decidim
   module HelsinkiSmsauth
     class SigninCodeSession < ApplicationRecord
-      belongs_to :user, foreign_key: "decidim_user_id", class_name: "Decidim::User"
+      belongs_to :user, -> { respond_to?(:entire_collection) ? entire_collection : self }, foreign_key: "decidim_user_id", class_name: "Decidim::User"
       belongs_to :signin_code_set, foreign_key: "decidim_signin_code_set_id", class_name: "::Decidim::HelsinkiSmsauth::SigninCodeSet"
     end
   end
