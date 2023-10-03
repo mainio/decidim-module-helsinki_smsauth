1c60f992e0bb97e4c5cf3ede0261c35e15a350f4
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-10-03 14:11:09 +0300

    Add tests for the code generation view in admin

--- spec/system/admin/verification_code_generation_spec.rb
+++ spec/system/admin/verification_code_generation_spec.rb
@@ -8,21 +8,28 @@ describe "verificqation code generation", type: :system do
   let!(:admin) { create(:user, :admin, :confirmed, organization: organization) }
 
   it_behaves_like "filterable login code"
-  xdescribe "login code generation" do
+  describe "login code generation" do
     before do
       switch_to_host(organization.host)
       login_as admin, scope: :user
       visit "admin/users"
     end
 
-    it "renders authorizations" do
+    it "renders authorizations views" do
       within ".secondary-nav" do
         expect(page).to have_link("Login via text message")
       end
 
       click_link("Login via text message")
-      expect(page).to have_current_path("admin/helsinki_smsauth_id/")
+      expect(page).to have_current_path("/admin/helsinki_smsauth_id/")
       expect(page).to have_content("Alternative login codes")
+
+      click_link "Create codes"
+      expect(page).to have_current_path("/admin/helsinki_smsauth_id/signin_codes/new")
+      expect(page).to have_content("Create codes")
+
+      click_button "Create"
+      expect(page).to have_content("is not included in the list")
     end
   end
 end
